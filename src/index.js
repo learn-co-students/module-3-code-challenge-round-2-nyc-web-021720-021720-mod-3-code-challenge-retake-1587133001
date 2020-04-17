df3333d65e1840fc5e3c57dbe1f48352505ad59c
Jared Fleming <jaredmf10@gmail.com>
2020-04-17 10:54:27 -0400

    working version

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // console.log('DOM fully loaded and parsed');
     let beerList = document.getElementById('beer-list')
     let beerUl = document.querySelector(".list-group")
-    console.log(beerList)
+    let beerDetail = document.getElementById('beer-detail')
+    // console.log(beerList)
     function doesMyFetch(){
         fetch('http://localhost:3000/beers')
         .then(response => response.json())
         .then((beers)=> {
             beers.forEach(beer => {
-                console.log(beer)
+                // console.log(beer)
                 let beerPanel = document.createElement('li')
                 beerPanel.className = "list-group-item"
                 beerPanel.id = beer.id
@@ -16,7 +17,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 beerUl.appendChild(beerPanel)
             });
 
-        
+        beerList.addEventListener("click", function (event){
+        let beerId = event.target.id
+        fetch(`http://localhost:3000/beers/${beerId}`)
+        .then(response => response.json())
+        .then((beer)=> {
+            beerDetail.innerHTML = `
+            <h1>${beer.name}</h1>
+            <img src="${beer.image_url}">
+            <h3>${beer.tagline}</h3>
+            <p>${beer.description}</p>
+            `
+            });
+        })
         });
     }
     doesMyFetch()
