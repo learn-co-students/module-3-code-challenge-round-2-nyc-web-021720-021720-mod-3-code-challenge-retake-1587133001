8ef073b20a4963cbfb03b6bbf1e615f01f0fd57f
github username <github email address>
2020-04-17 11:13:56 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const url = "http://localhost:3000/beers";
 const ul = document.getElementById("beer-list");
 let beerPanel = document.getElementById("beer-detail");
-const beerList = [];
 function getBeers() {
 	fetch(url)
 		.then((response) => response.json())
 		.then((beers) => {
 			beers.forEach(function (beer) {
-				beerList.push(beer);
 				renderBeer(beer);
 			});
 		});
@@ -25,24 +23,16 @@ function renderBeer(beer) {
 
 ul.addEventListener("click", function (event) {
 	if (event.target.className === "list-group-item") {
-		beerList.forEach(function (beer) {
-			if (beer.id == event.target.dataset.id) {
-				console.log(beer);
-				console.log(beerPanel);
+		fetch(`http://localhost:3000/beers/${event.target.dataset.id}`)
+			.then((response) => response.json())
+			.then((beer) => {
 				beerPanel.innerHTML = `
-                <h1>Beer Name: ${beer.name}</h1>
-                <img src="${beer.image_url}">
-                <h3>Beer Tagline: ${beer.tagline}</h3>
-                <p>Beer Description: ${beer.description}</p>
-        `;
-				console.log(beerPanel);
-			}
-		});
-		// fetch(`http://localhost:3000/beers/${event.target.dataset.id}`)
-		// 	.then((response) => response.json())
-		// 	.then(console.log(response));
-		// then((beer) => {
-		// 	// });
+                    <h1>Beer Name: ${beer.name}</h1>
+                    <img src="${beer.image_url}">
+                    <h3>Beer Tagline: ${beer.tagline}</h3>
+                    <p>Beer Description: ${beer.description}</p>
+            `;
+			});
 	}
 });
 getBeers();
