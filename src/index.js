8df253ccd5ddb1d3a4b88d887f588c24f5a63dcc
Ty <tyreewilliams515@gmail.com>
2020-04-17 11:22:02 -0400

    partdone

--- src/index.js
+++ src/index.js
@@ -4,26 +4,25 @@ Document.addEventListener('DOMContentLoaded', (event) => {
             return response.json()
         })
         .then((data) => {
-            let beers = data.results
+                let beers = data.results
             return beers.map(function(beers) {
-                let li = createNode('li')
-                let h1 = createNode('h1')
-                let img = createNode('img')
-                let h3 = createNode('h3')
-                let p = createNode('p')
-                let div = Document.querySelector('beer-detail')
-                h3.innerHTML = `${beers.tagline}`
-            img.src = beers.image_url 
-            h1.innerHTML = `${beers.name}` 
-            p.innerHTML = `${beers.description}`
-            append(li, h1)
-            append(li, img)
-            append(li, h3)
-            append(li, p)
-            append(div, li)
-            }
-        }
-});
+                    let li = createNode('li')
+                    let h1 = createNode('h1')
+                    let img = createNode('img')
+                    let h3 = createNode('h3')
+                    let p = createNode('p')
+                    let div = Document.querySelector('beer-detail')
+                    h3.innerHTML = `${beers.tagline}`
+                img.src = beers.image_url 
+                h1.innerHTML = `${beers.name}` 
+                p.innerHTML = `${beers.description}`
+                append(li, h1)
+                append(li, img)
+                append(li, h3)
+                append(li, p)
+                append(div, li)
+            })
+})
 
 function createNode(element) {
     return document.createElement(element)
