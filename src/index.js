2687cdfa681b2914e4bbb2c822e1137e927ea841
Ty <tyreewilliams515@gmail.com>
2020-04-17 11:19:15 -0400

    part3

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-fetch('http://localhost:3000/beers')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let beers = data.results;
-    return beers.map(function(beers) {
-        let h1 = createNode('h1');
-        let img = createNode('img');
-        let h3 = createNode('h3');
-        let p = createNode('p');
-        let div = findElementByID()
-      img.src = beers.picture.medium; 
-      h3.innerHTML = `${beers.name.first} ${beers.name.last}`; 
-      append(div, img);
-      append(, span);
-      append(ul, );
-
-      function createNode(element) {
-        return document.createElement(element);
-      }
-    
-      function append(parent, el) {
-        return parent.appendChild(el);
-      }
+Document.addEventListener('DOMContentLoaded', (event) => {
+        fetch('http://localhost:3000/beers')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let beers = data.results
+            return beers.map(function(beers) {
+                let li = createNode('li')
+                let h1 = createNode('h1')
+                let img = createNode('img')
+                let h3 = createNode('h3')
+                let p = createNode('p')
+                let div = Document.querySelector('beer-detail')
+                h3.innerHTML = `${beers.tagline}`
+            img.src = beers.image_url 
+            h1.innerHTML = `${beers.name}` 
+            p.innerHTML = `${beers.description}`
+            append(li, h1)
+            append(li, img)
+            append(li, h3)
+            append(li, p)
+            append(div, li)
+            }
+        }
 });
 
-//copied from https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data but edited and added stuff
+function createNode(element) {
+    return document.createElement(element)
+}
+
+function append(parent, el) {
+    return parent.appendChild(el)
+}
